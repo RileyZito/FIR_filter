51eb7ec1d4ab382a900c1fda305292f22809df4e
Duncan Mazza <dmazza@olin.edu>
2021-12-09 20:09:52 -0500

    Improve documentation and regex

--- parse_output.py
+++ parse_output.py
@@ -7,15 +7,22 @@ now_fmt = "%y-%M-%d-%h-%m-%s"
 
 
 def main():
-    # Loop will run until eof
+    # Make a results folder if one does not exist
     if not os.path.exists("results"):
         os.mkdir("results")
 
+    # Open a new file in the results folder whose name contains the date and time
     with open(os.path.join("results", f"results_{datetime.datetime.now().strftime(now_fmt)}.csv"), "w") as f:
+        # Loop will run until eof
         for line in fileinput.input():
-            print(line, end="")
-            if len(re.findall(r"[\s\d]+,[\s\d]+", line)) != 1:
+            print(line, end="")  # echo the output to the terminal
+
+            # Check to see if the line contains two integers separated by a comma (and with arbitrary whitespace
+            # padding)
+            if len(re.findall(r"^[\s\d]+,[\s\d]+$", line)) != 1:
                 continue
+
+            # Match the integers and concatenate them into a new line in the output file
             values = re.findall(r"((?=\s*)\d+)", line)
             f.writelines([",".join(values) + "\n"])
 
