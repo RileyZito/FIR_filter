a75b4eca8a2404bf15693d79c837f7ed62ec112d
Duncan Mazza <dmazza@olin.edu>
2021-12-09 20:03:12 -0500

    Update testbench to fix bugs and print output

--- test_fir_n.sv
+++ test_fir_n.sv
@@ -11,6 +11,7 @@ parameter N = 32;  // Signal wire size
 parameter DELAYS = 3;  // Number of z^-1's (number of filter blocks is +1)
 
 int errors = 0;
+logic is_simulating;
 
 wire [(DELAYS + 1) * N - 1:0] b;
 assign b = {{32'd1}, {32'd2}, {32'd3}, {32'd4}};
@@ -38,37 +39,47 @@ fir_n #(.DELAYS(DELAYS), .N(N)) UUT(
   .rst(rst)
 );
 
-// task print_io;
-//  $display()
-// endtask
-
 // Run our main clock.
 always #(CLK_PERIOD_NS/2) clk = ~clk;
 
 // 2) the test cases
 initial begin
+  
   $dumpfile("fir_n.vcd");
   $dumpvars(0, UUT);
+  
+  is_simulating = 1'b0;
+  x_in = {N{1'b0}};
     
   rst = 1;
   ena = 1;
   clk = 0;
-  repeat(2) @(negedge clk);
-  rst = 0;
   repeat(2) @(posedge clk);
+  rst = 0;
+  repeat(2) @(posedge clk_d);
+
+  is_simulating = 1'b1;
+  
+
+  repeat(5) @(posedge clk_d);
 
   // Generate impulse
-  x_in = {N{1'b0}};
   repeat(1) @(posedge clk_d);
-  x_in = 32'd101;
+  x_in = 32'b11111111;
   repeat(1) @(posedge clk_d);
   x_in = {N{1'b0}};
   repeat(1) @(posedge clk_d);
 
-  // Let the response play out 
+  // Let the response play out
   repeat(20) @(posedge clk_d);
 
   $finish;
+end
+
+always @(posedge clk_d) begin
+  if (is_simulating) begin
+    UUT.print_io();
+  end
 end
 
 endmodule
